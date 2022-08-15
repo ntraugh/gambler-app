235f0a2b699731ab74beb3035e7b2307b73487d2
Nate Traugh <n.traugh@yahoo.com>
2022-08-15 13:33:02 -0400

    added bootstrap to the project

--- src/components/BetCardItem.js
+++ src/components/BetCardItem.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import {Button} from 'react-bootstrap'
+import {Button, Col, Row} from 'react-bootstrap'
 
 const BetCardItem = ({game}) => {
+    // somehow filter by best odds
+
   return (
-    <div>
-        <p>{game.home_team}</p>
-        VS.
-        <p>{game.away_team}</p>
-        <Button a href={`https://www.google.com/search?q=${game.bookmakers[5].title} +${game.home_team}`}>{game.bookmakers[5].title}</Button>
-    </div>
+    <Row>
+        <Col>
+            <p>{game.home_team}</p>
+            VS.
+            <p>{game.away_team}</p>
+            <Button variant="primary" a href={`https://www.google.com/search?q=${game.bookmakers[0].title} +${game.home_team}`}>{game.bookmakers[0].title}</Button>
+            <Button a href={`https://www.google.com/search?q=${game.bookmakers[1].title} +${game.home_team}`}>{game.bookmakers[1].title}</Button>
+            <Button a href={`https://www.google.com/search?q=${game.bookmakers[2].title} +${game.home_team}`}>{game.bookmakers[2].title}</Button>
+        </Col>
+    </Row>
   )
 }
 
